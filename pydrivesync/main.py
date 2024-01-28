76251013953a3cbb71e6cfbbade26a5bd1a02ba0
Alexandre Wauquier <alleyne234@outlook.fr>
2024-01-28 22:23:30 +0100

    Refactoring of main.py

--- pydrivesync/main.py
+++ pydrivesync/main.py
@@ -9,25 +9,27 @@ folder_download = 'Downloads'
 folder_upload = 'Uploads'
 folder_instruction = 'Instructions'
 folder_token = 'Tokens'
+token_path = "Tokens/token.json"
 
 
 # Function to authenticate the Drive service
 def authenticate():
-    service = None
-    while service is None:
+    while True:
         service = get_authenticated_drive_service()
-        if service is None:
-            if os.path.exists("Tokens/token.json"):
-                retry = input("Authentication failed. Type 'del' to delete 'token.json' and retry or 'quit' to exit.\n")
-                if retry.lower() == 'del':
-                    os.remove("Tokens/token.json")
-            else:
-                retry = input("Authentication failed. Press Enter to retry or 'quit' to exit.\n")
-            if retry.lower() == 'quit':
-                input("Press any key to continue...")
-                exit()
 
-    return service
+        if service is not None:
+            return service
+        
+        if os.path.exists(token_path):
+            user_input = input("Authentication failed. Type 'del' to delete 'token.json' and retry or 'quit' to exit.\n")
+            if user_input.lower() == 'del':
+                os.remove(token_path)
+        else:
+            user_input = input("Authentication failed. Press Enter to retry or 'quit' to exit.\n")
+
+        if user_input.lower() == 'quit':
+            input("Press any key to continue...")
+            exit()
 
 
 # Function to create necessary folders if they don't exist
@@ -39,11 +41,13 @@ def create_folders_if_not_exist(*folders):
 
 def is_valid_folder_name(name):
     invalid_characters = re.compile(r'[<>:"/\\|?*\x00-\x1F]')
+
     if invalid_characters.search(name) is not None:
         return False
     
     reserved_names = ['CON', 'PRN', 'AUX', 'NUL', 'COM1', 'COM2', 'COM3', 'COM4', 'COM5', 'COM6', 'COM7', 'COM8', 'COM9',
                       'LPT1', 'LPT2', 'LPT3', 'LPT4', 'LPT5', 'LPT6', 'LPT7', 'LPT8', 'LPT9']
+    
     if name.upper() in reserved_names:
         return False
 
@@ -56,13 +60,15 @@ def create_folder_option(service):
         folder_name = input("Enter the name of the folder (press Enter to return to the menu)\n")
         if folder_name == "":
             return
-        elif is_valid_folder_name(folder_name):
-            parent_folder_id = input("Enter the ID of the destination folder (leave empty to create a folder at the root):\n")
-            create_folder(service, folder_name, parent_folder_id)
-            input("Press any key to continue...")
-            break
-        else:
+        
+        if not is_valid_folder_name(folder_name):
             print("Invalid folder name. Please provide a different name.")
+            continue
+
+        parent_folder_id = input("Enter the ID of the destination folder (leave empty to create a folder at the root):\n")
+        create_folder(service, folder_name, parent_folder_id)
+        input("Press any key to continue...")
+        return
     
 
 # Option 2 - 'Display tree structure'
@@ -72,13 +78,13 @@ def display_tree_option(service):
         return
 
     try:
-        if folder_id == "":
-            display_drive_tree(service)
-        else:
+        if folder_id:
             display_drive_tree(service, folder_id)
+        else:
+            display_drive_tree(service)
+        input("Press any key to continue...")
     except Exception as e:
         print(f"An error occurred: {e}")
-    input("Press any key to continue...")
 
 
 # Option 3 - 'Display folders and files with ID'
@@ -86,15 +92,17 @@ def display_drive_items_option(service):
     while True:
         folder_id = input("What is the ID of the folder you are listing? (press Enter to list all items, type 'quit' to return to the menu)\n")
         if folder_id.lower() == 'quit':
-            break
-        elif folder_id:
-            display_drive_items(service, folder_id)
+            return
+        
+        try:
+            if folder_id:
+                display_drive_items(service, folder_id)
+            else:
+                display_drive_items(service)
             input("Press any key to continue...")
-            break
-        else:
-            display_drive_items(service)
-            input("Press any key to continue...")
-            break
+            return
+        except Exception as e:
+            print(f"An error occurred: {e}")
 
 
 # Option 4 - 'Download file or folder and its contents'
@@ -102,20 +110,22 @@ def download_option(service):
     while True:
         item_id = input("What's the ID of the file or folder? (type 'quit' to return to the menu)\n")
         if item_id.lower() == 'quit':
-            break
-        elif item_id:
-            try:
-                destination = input("What's the path of the destination? (press Enter to use default path)\n")
-                if destination:
-                    download(service, item_id, destination)
-                else:
-                    download(service, item_id, download_folder)
-                input("Press any key to continue...")
-                break
-            except Exception as e:
-                print(f"An error occurred: {e}")
-        else:
+            return
+        
+        if not item_id:
             print("Please provide an ID.")
+            continue
+
+        try:
+            destination = input("What's the path of the destination? (press Enter to use default path)\n")
+            if destination:
+                download(service, item_id, destination)
+            else:
+                download(service, item_id, download_folder)
+            input("Press any key to continue...")
+            return
+        except Exception as e:
+            print(f"An error occurred: {e}")
 
 
 # Option 5 - 'Upload file or folder and its contents'
@@ -123,20 +133,23 @@ def upload_option(service):
     while True:
         items_path = input("What's the path of the file or folder? (type 'quit' to return to the menu)\n")
         if items_path.lower() == 'quit':
-            break
-        elif items_path:
-            if os.path.exists(items_path):
-                destination = input("What's the destination? (press Enter to use root path)\n")
-                if destination:
-                    upload(service, items_path, destination)
-                else:
-                    upload(service, items_path)
-                input("Press any key to continue...")
-                break
-            else:
-                print("An error occurred, the file or folder does not exist or is inaccessible.")
-        else:
+            return
+        
+        if not items_path:
             print("Please provide a file or folder path.")
+            continue
+        
+        if not os.path.exists(items_path):
+            print("An error occurred, the file or folder does not exist or is inaccessible.")
+            continue
+
+        destination = input("What's the destination? (press Enter to use root path)\n")
+        if destination:
+            upload(service, items_path, destination)
+        else:
+            upload(service, items_path)
+        input("Press any key to continue...")
+        return
 
 
 # Option 6 - 'Process instructions'
@@ -152,8 +165,8 @@ def process_instructions(service):
     
     while True:
         print("Available instruction files:")
-        for idx, file_path in enumerate(instruction_files):
-            print(f"{idx + 1}. {os.path.basename(file_path)}")
+        for file_index, file_path in enumerate(instruction_files):
+            print(f"{file_index + 1}. {os.path.basename(file_path)}")
         
         choice = input("Enter the number of the instruction file to execute (Press Enter to return to the menu):\n")
         if choice == "":
@@ -161,23 +174,26 @@ def process_instructions(service):
         
         try:
             choice_index = int(choice) - 1
-            if 0 <= choice_index < len(instruction_files):
-                chosen_file = instruction_files[choice_index]
-                instructions = read_instructions(chosen_file)
-                upload_instructions = instructions.get('UPLOAD', [])
-                download_instructions = instructions.get('DOWNLOAD', [])
-                
-                for upload_task in upload_instructions:
-                    upload(service, upload_task['source'], upload_task['destination'])
-                
-                for download_task in download_instructions:
-                    download(service, download_task['source'], download_task['destination'])
-                
-                print("End Uploads and Downloads tasks.")
-                input("Press any key to continue...")
-                break
-            else:
+
+            if choice_index < 0 or choice_index >= len(instruction_files):
                 print("Invalid choice. Please enter a number within the range.")
+                continue
+
+            chosen_file = instruction_files[choice_index]
+            instructions = read_instructions(chosen_file)
+            upload_instructions = instructions.get('UPLOAD', [])
+            download_instructions = instructions.get('DOWNLOAD', [])
+            
+            for upload_task in upload_instructions:
+                upload(service, upload_task['source'], upload_task['destination'])
+            
+            for download_task in download_instructions:
+                download(service, download_task['source'], download_task['destination'])
+            
+            print("End Uploads and Downloads tasks.")
+            input("Press any key to continue...")
+            return
+
         except ValueError:
             print("Invalid choice. Please enter a valid number.")
 
